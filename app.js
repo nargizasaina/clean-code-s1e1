278614fd6c0b2fc8361489349e6a44b54ce19961
Nargiza Sainazarova <nargizasaina@gmail.com>
2023-04-20 15:21:43 +0600

    refactor(extended-1.2): add alt

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var createNewTaskElement=function(taskString){
   //button.delete
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
+  deleteButtonImg.alt = "remove button";
   label.innerText=taskString;
   label.className="task";
   //Each elements, needs appending
